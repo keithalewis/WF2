ddfffe1eaa6e2a70672240e5a9525395e724b518
Keith A. Lewis <kal@kalx.net>
2024-07-21 16:37:49 -0400

    remove numpy

--- Matrix.h
+++ Matrix.h
@@ -6,8 +6,10 @@
 #include <span>
 #include <string>
 #include <valarray>
+#include <vector>
 
 extern "C" {
+
     inline void setUpLibrary()
     {
         puts("setUpLibrary");
@@ -21,22 +23,43 @@ extern "C" {
 namespace WF2 {
 
 	struct Matrix {
-		int rows;
-		int columns;
+		size_t rows;
+		size_t columns;
 		std::valarray<double> array;
 
-		Matrix(int rows = 1, int columns = 1, double value = 0)
+		Matrix(size_t rows = 1, size_t columns = 1, double value = 0)
 			: rows(rows), columns(columns), array(value, rows*columns)
 		{ }
 		// Construct from array of doubles.
 		// Assumes array points at rows * columns doubles
-		Matrix(int rows, int columns, double* array)
+		Matrix(size_t rows, size_t columns, double* array)
 			: rows(rows), columns(columns), array(array, rows*columns)
 		{ }
+        // Construct from ragged vector SWIG knows about.
+        Matrix(const std::vector<std::vector<double>>& vv)
+        {
+            size_t r = vv.size();
+            size_t c = r > 0 ? vv[0].size() : 0;
+            if (r*c == 0) {
+                throw "Matrix: size must be positive";
+            }
+
+            // Flatten ragged vector.
+            std::vector<double> v = vv[0];
+            for (size_t i = 1; i < r; ++i) {
+                if (vv[i].size() != r) {
+                    throw "Matrix: all rows must be the same size";
+                }
+                v.insert(v.end(), vv[i].begin(), vv[i].end());
+            }
+
+            operator=(Matrix(r, c, v.data()));
+
+        }
 		// Read from parameter file.
 		explicit Matrix(const char* path)
 		{
-			int rows, columns;
+			size_t rows, columns;
 			double value;
 
 			std::ifstream ifs(path);
@@ -66,11 +89,11 @@ namespace WF2 {
 
             oss << "[";
             auto commai = "";
-            for (int i = 0; i < rows; ++i) {
+            for (size_t i = 0; i < rows; ++i) {
               oss << commai << "[";
               commai = ", ";
               auto commaj = "";
-              for (int j = 0; j < columns; ++j) {
+              for (size_t j = 0; j < columns; ++j) {
                 oss << commaj << array[i * columns + j];
                 commaj = ", ";
               }
--- Matrix.i
+++ Matrix.i
@@ -6,65 +6,15 @@
 %}
 
 %include "typemaps.i"
-%include "numpy.i"
+%include "std_vector.i"
 
-%init %{
-import_array();
-%}
+namespace std {
+%template(Line) vector<double>;
+    %template(Array) vector<vector<double>>;
+}
+
 
 %include "Matrix.h"
-%apply (int DIM1, int DIM2, double* IN_ARRAY2) {(int r, int c, double* a)};
 
 extern void setUpLibrary();
 extern void tearDownLibrary();
-/*
-%typemap(in) double matrix[ANY][ANY] (double temp[$1_dim0][$1_dim1]) {
-  PyObject *resultobj = 0;
-  int arg1 ;
-  int arg2 ;
-  double *arg3 = (double *) 0 ;
-  int val1 ;
-  int ecode1 = 0 ;
-  int val2 ;
-  int ecode2 = 0 ;
-  void *argp3 = 0 ;
-  int res3 = 0 ;
-  WF2::Matrix *result = 0 ;
-
-  if (!PySequence_Check($input)) {
-    PyErr_SetString(PyExc_ValueError, "Expected a sequence");
-    SWIG_fail;
-  }
-  if (PySequence_Length($input) != $1_dim0) {
-    PyErr_SetString(PyExc_ValueError, "Size mismatch. Expected $1_dim0 elements");
-    SWIG_fail;
-  }
-  // Rectangular array.
-  for (Py_ssize_t i = 0; i < $1_dim0; ++i) {
-    if (PySequence_Length(PySequence_GetItem($input, i) != $1_dim1) {
-      PyErr_SetString(PyExc_ValueError, "Size mismatch. Expected $1_dim1 elements");
-      SWIG_fail;
-    }
-  }
-
-  std::vector<double> array[$1_dim0 * $1_dim1];
-
-  for (Py_ssize_t i = 0; i < $1_dim0; ++i) {
-    for (Py_ssize_t j = 0; j < $1_dim1; ++j) {
-        PyObject* oi = PySequence_GetItem($input, i);
-        PyObject* oij = PySequence_GetItem(oi, j);
-        if (PyNumber_Check(oij)) {
-            array[i*$1_dim1 + j] = PyFloat_AsDouble(o);
-    } else {
-      PyErr_SetString(PyExc_ValueError, "Sequence elements must be numbers");
-      SWIG_fail;
-    }
-  }
-
-  result = (WF2::Matrix *)new WF2::Matrix($1_dim0,$1_dim1,array.data());
-  resultobj = SWIG_NewPointerObj(SWIG_as_voidptr(result), SWIGTYPE_p_WF2__Matrix, SWIG_POINTER_NEW |  0 );
-  return resultobj;
-fail:
-  return NULL;
-}
-*/
